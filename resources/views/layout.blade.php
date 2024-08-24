2fba5eba80d98ae1d3bf6a70a980e6af59e438f5
Iqbal-Maulana67 <fikri.rid@gmail.com>
2024-08-24 12:36:12 +0700

    Adding templates

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,240 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ADS Intership Test</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/backend-plugin.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/backend.css?v=1.0.0') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/remixicon/fonts/remixicon.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/summernote/summernote.min.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>>
+
+<body class="">
+    <!-- loader Start -->
+    <div id="loading">
+        <div id="loading-center">
+        </div>
+    </div>
+    <!-- loader END -->
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+
+        <div class="iq-sidebar sidebar-default">
+            <div class="iq-sidebar-logo d-flex align-items-center justify-content-between">
+                <a href="../backend/index.html" class="header-logo">
+                    <img src="../assets/images/logo.png" class="img-fluid rounded-normal light-logo" alt="logo" />
+                    <h5 class="logo-title light-logo ml-3">POSDash</h5>
+                </a>
+                <div class="iq-menu-bt-sidebar ml-0">
+                    <i class="las la-bars wrapper-menu"></i>
+                </div>
+            </div>
+            <div class="data-scrollbar" data-scroll="1">
+                <nav class="iq-sidebar-menu">
+                    <ul id="iq-sidebar-toggle" class="iq-menu">
+                        <li class="">
+                            <a href="../backend/index.html" class="svg-icon">
+                                <svg class="svg-icon" id="p-dash1" width="20" height="20"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path
+                                        d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z">
+                                    </path>
+                                    <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
+                                    <line x1="12" y1="22.08" x2="12" y2="12"></line>
+                                </svg>
+                                <span class="ml-4">Dashboards</span>
+                            </a>
+                        </li>
+                        <li class="">
+                            <a href="../backend/page-report.html" class="">
+                                <svg class="svg-icon" id="p-dash7" width="20" height="20"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"
+                                    stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round">
+                                    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                                    <polyline points="14 2 14 8 20 8"></polyline>
+                                    <line x1="16" y1="13" x2="8" y2="13"></line>
+                                    <line x1="16" y1="17" x2="8" y2="17"></line>
+                                    <polyline points="10 9 9 9 8 9"></polyline>
+                                </svg>
+                                <span class="ml-4">Reports</span>
+                            </a>
+                            <ul id="reports" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle"></ul>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="p-3"></div>
+            </div>
+        </div>
+        <div class="iq-top-navbar">
+            <div class="iq-navbar-custom">
+                <nav class="navbar navbar-expand-lg navbar-light p-0">
+                    <div class="iq-navbar-logo d-flex align-items-center justify-content-between">
+                        <i class="ri-menu-line wrapper-menu"></i>
+                        <a href="../backend/index.html" class="header-logo">
+                            <img src="../assets/images/logo.png" class="img-fluid rounded-normal" alt="logo">
+                            <h5 class="logo-title ml-3">POSDash</h5>
+
+                        </a>
+                    </div>
+                    <div class="d-flex align-items-center w-100">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-label="Toggle navigation">
+                            <i class="ri-menu-3-line"></i>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav ml-auto navbar-list align-items-center">
+                                    <div class="iq-sub-dropdown dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <div class="card shadow-none m-0">
+                                            <div class="card-body p-0 ">
+                                                <div class="cust-title p-3">
+                                                    <div class="d-flex align-items-center justify-content-between">
+                                                        <h5 class="mb-0">Notifications</h5>
+                                                        <a class="badge badge-primary badge-card"
+                                                            href="#">3</a>
+                                                    </div>
+                                                </div>
+                                                <div class="px-3 pt-0 pb-0 sub-card">
+                                                    <a href="#" class="iq-sub-card">
+                                                        <div
+                                                            class="media align-items-center cust-card py-3 border-bottom">
+                                                            <div class="">
+                                                                <img class="avatar-50 rounded-small"
+                                                                    src="../assets/images/user/01.jpg"
+                                                                    alt="01">
+                                                            </div>
+                                                            <div class="media-body ml-3">
+                                                                <div
+                                                                    class="d-flex align-items-center justify-content-between">
+                                                                    <h6 class="mb-0">Emma Watson</h6>
+                                                                    <small class="text-dark"><b>12 : 47 pm</b></small>
+                                                                </div>
+                                                                <small class="mb-0">Lorem ipsum dolor sit
+                                                                    amet</small>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                    <a href="#" class="iq-sub-card">
+                                                        <div
+                                                            class="media align-items-center cust-card py-3 border-bottom">
+                                                            <div class="">
+                                                                <img class="avatar-50 rounded-small"
+                                                                    src="../assets/images/user/02.jpg"
+                                                                    alt="02">
+                                                            </div>
+                                                            <div class="media-body ml-3">
+                                                                <div
+                                                                    class="d-flex align-items-center justify-content-between">
+                                                                    <h6 class="mb-0">Ashlynn Franci</h6>
+                                                                    <small class="text-dark"><b>11 : 30 pm</b></small>
+                                                                </div>
+                                                                <small class="mb-0">Lorem ipsum dolor sit
+                                                                    amet</small>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                    <a href="#" class="iq-sub-card">
+                                                        <div class="media align-items-center cust-card py-3">
+                                                            <div class="">
+                                                                <img class="avatar-50 rounded-small"
+                                                                    src="../assets/images/user/03.jpg"
+                                                                    alt="03">
+                                                            </div>
+                                                            <div class="media-body ml-3">
+                                                                <div
+                                                                    class="d-flex align-items-center justify-content-between">
+                                                                    <h6 class="mb-0">Kianna Carder</h6>
+                                                                    <small class="text-dark"><b>11 : 21 pm</b></small>
+                                                                </div>
+                                                                <small class="mb-0">Lorem ipsum dolor sit
+                                                                    amet</small>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                                <a class="right-ic btn btn-primary btn-block position-relative p-2"
+                                                    href="#" role="button">
+                                                    View All
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li class="nav-item nav-icon dropdown caption-content">
+                                    <a href="#" class="search-toggle dropdown-toggle"
+                                        id="dropdownMenuButton4" data-toggle="dropdown" aria-haspopup="true"
+                                        aria-expanded="false">
+                                        <img src="../assets/images/user/1.png" class="img-fluid rounded"
+                                            alt="user">
+                                    </a>
+                                    <div class="iq-sub-dropdown dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <div class="card shadow-none m-0">
+                                            <div class="card-body p-0 text-center">
+                                                <div class="media-body profile-detail text-center">
+                                                    <img src="../assets/images/page-img/profile-bg.jpg" alt="profile-bg"
+                                                        class="rounded-top img-fluid mb-4">
+                                                    <img src="../assets/images/user/1.png" alt="profile-img"
+                                                        class="rounded profile-img img-fluid avatar-70">
+                                                </div>
+                                                <div class="p-3">
+                                                    <h5 class="mb-1">JoanDuo@property.com</h5>
+                                                    <p class="mb-0">Since 10 march, 2020</p>
+                                                    <div class="d-flex align-items-center justify-content-center mt-3">
+                                                        <a href="../app/user-profile.html" class="btn border mr-2">Profile</a>
+                                                        <a href="auth-sign-in.html" class="btn border">Sign Out</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+        <div class="content-page">
+            <div class="container-fluid">
+                @yield('content')
+            </div>
+        </div>
+    </div>
+
+    <!-- Backend Bundle JavaScript -->
+    <script src="{{ asset('assets/js/backend-bundle.min.js') }}"></script>
+
+    <!-- Table Treeview JavaScript -->
+    <script src="{{ asset('assets/js/table-treeview.js') }}"></script>
+
+    <!-- Chart Custom JavaScript -->
+    <script src="{{ asset('assets/js/customizer.js') }}"></script>
+
+    <!-- Chart Custom JavaScript -->
+    <script async src="{{ asset('assets/js/chart-custom.js') }}"></script>
+
+    <!-- app JavaScript -->
+    <script src="{{ asset('assets/js/app.js') }}"></script>
+
+    {{-- Summernote script --}}
+    <script src="{{ asset('assets/vendor/summernote/summernote.js') }}"></script>
+
+
+    @yield('js-script')
+
+</body>
+
+</html>
---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-4">
+        <div class="card card-block card-stretch card-height-helf">
+            <div class="card-body card-item-right">
+                <div class="d-flex align-items-top">
+                    <div class="bg-warning-light rounded">
+                        <img src="../assets/images/product/04.png" class="style-img img-fluid m-auto" alt="image">
+                    </div>
+                    <div class="style-text text-left">
+                        <h5 class="mb-2"></h5>
+                        <p class="mb-2"></p>
+                        <p class="mb-0"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
